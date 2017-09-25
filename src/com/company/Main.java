9c85defa0ed00d05ca35ee25e061d0952b9afa50
IC_104022064 <atim0981336515@gmail.com>
2017-09-25 14:43:14 +0800

    modified

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("104022064,石庭碩");
+        System.out.println("商應係");
+
+    }
+}
